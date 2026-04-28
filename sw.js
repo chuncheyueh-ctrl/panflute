0dcbc49aca687ff367e027b8760cb058aa13333a
chuncheyueh-ctrl <chuncheyueh@gmail.com>
2026-04-28 18:59:56 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
+const CACHE_NAME = "battle-panflute-v13";
+const FILES = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
-  // V14.1: do not intercept requests. Always load current GitHub Pages files.
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
