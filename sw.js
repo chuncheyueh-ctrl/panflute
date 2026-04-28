722f342e7017484a1a63bf51f4b34c77762e3260
chuncheyueh-ctrl <chuncheyueh@gmail.com>
2026-04-29 07:49:56 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,5 +3,5 @@ self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener("fetch", event => {
-  // V17: no cache interception.
+  // V21.1: no cache interception.
 });
