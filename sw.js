edbc5e91437031774416daf4dab6b9e6a177f1f8
chuncheyueh-ctrl <chuncheyueh@gmail.com>
2026-04-30 14:00:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-self.addEventListener("install", event => { self.skipWaiting(); });
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
-self.addEventListener("fetch", event => {});
+// V34: service worker disabled intentionally to avoid cache issues.
