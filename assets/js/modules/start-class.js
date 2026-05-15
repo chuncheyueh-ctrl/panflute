bfad4885ef06e3cc8d96904d9c4ec756876bbe2b
chuncheyueh-ctrl <chuncheyueh@gmail.com>
2026-05-15 19:46:32 +0800

    Delete assets directory

--- assets/js/modules/start-class.js
+++
@@ -1,5 +0,0 @@
-/* start-class.js | v8.1
-   開始上課模組預留檔。
-   開始上課模式保留於 app-main.js，後續可在此獨立維護點名、XP、課堂備註。 */
-window.BattleModules = window.BattleModules || {};
-window.BattleModules.startClass = { status: "loaded" };
